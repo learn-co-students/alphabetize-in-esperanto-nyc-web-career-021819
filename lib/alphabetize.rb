419a4f022c8953dd8cd56a0085d3b0ac153981a1
Nicholas Dover <dovern42@gmail.com>
2019-02-16 19:28:05 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,15 @@
+
+
+
+#build a function that returns its place in the alphabet
+def arrayplace (letter)
+  array= "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".split
+  array
+end
+
+arrayplace("g")
+
+
 def alphabetize(arr)
   # code here
 end
